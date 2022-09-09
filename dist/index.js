3ab55c20fc2d1e4cb1e3c0648fe04e4ee6f79a77
Sun Dong <57502391+sundong-exideatech@users.noreply.github.com>
2022-09-09 11:11:05 +0800

    update sdk

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./sdkuni.cjs.production.min.js')
-} else {
-  module.exports = require('./sdkuni.cjs.development.js')
-}
