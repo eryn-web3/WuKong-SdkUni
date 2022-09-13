884d9fd595a07544578698f866900f9cb411eff3
Sun Dong <57502391+sundong-exideatech@users.noreply.github.com>
2022-09-14 01:51:39 +0800

    add rinkeby network

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -66,7 +66,7 @@ export const WETH = {
   ),
   [ChainId.RINKEBY]: new Token(
     ChainId.RINKEBY,
-    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
     18,
     'WETH',
     'Wrapped ETH'
